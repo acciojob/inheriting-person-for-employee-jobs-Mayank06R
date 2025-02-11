9bcf8f2a87858767e23903234f3f65fa12a036aa
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-11 10:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ class Employee extends Person {
         super(name, age);
         this.jobTitle = jobTitle;
     }
-
-	 jobGreet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-    }
+	
+    Employee.prototype.jobGreet=function() {
+        (`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+		}	
+    
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
